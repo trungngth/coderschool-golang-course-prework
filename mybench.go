9fe690cd945f8d3bb1352937f7046ed10d1fa5bc
Vincent <vincentnguyenthanhtrung@gmail.com>
2019-04-13 10:16:09 -0500

    Set up requests and concurrency flag

--- mybench.go
+++ mybench.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello from my app")
+
+	requests := flag.Int64("n", 1, "Number of requests to perform")
+	concurrency := flag.Int64("c", 1, "Number of multiple requests to make at a time")
+
+	fmt.Println(&requests, &concurrency)
+	fmt.Println(*requests, *concurrency)
+	flag.Parse()
+	flag.PrintDefaults()
 }
